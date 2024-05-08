d26491e083f8aaa9d9229700c8148904da800dda
Guilherme Eduardo <guilhermeeduardo2016@outlook.com>
2024-05-08 00:49:50 -0300

    feat: conclusão do carrossel e inicio do conteudo do cardapio

--- js/script.js
+++ js/script.js
@@ -89,3 +89,79 @@ setInterval(function () {
     contador = 1;
   }
 }, 2000);
+
+//carrossel_two 
+
+const initSlider = () => {
+  const carrosselTwoSlidesImgList = document.querySelector('.carrossel__two__slides .carrossel_two_slides__img__list');
+  const slideButtons = document.querySelectorAll('.carrossel__two__slides .slide-button');
+  const sliderScrollbar = document.querySelector('.carrossel__two .slider-scrollbar');
+  const scrollbarThumb = sliderScrollbar.querySelector('.scrollbar-thumb');
+  const maxScrollLeft = carrosselTwoSlidesImgList.scrollWidth - carrosselTwoSlidesImgList.clientWidth;
+
+  scrollbarThumb.addEventListener('mousedown', (e) => {
+    const startX = e.clientX;
+    const thumbPosition = scrollbarThumb.offsetLeft;
+
+    const handleMouseMove = (e) => {
+    const deltaX = e.clientX -  startX;
+    const newThumbPosition = thumbPosition + deltaX;
+    const maxThumbPosition = sliderScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetWidth;
+
+
+    const boundedPosition = Math.max(0, Math.min(maxThumbPosition, newThumbPosition));
+    const scrollPosition = (boundedPosition / maxThumbPosition ) * maxScrollLeft;
+    
+    
+    scrollbarThumb.style.left = `${boundedPosition}px`;
+    carrosselTwoSlidesImgList.scrollLeft = scrollPosition;
+    }
+
+    const handleMouseUp = () => { 
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
+    }
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  });
+
+    slideButtons.forEach(button => {
+      button.addEventListener('click', () => {
+       const direction = button.id === "prev-slide" ? -1 : 1;
+       const scrollAmount = carrosselTwoSlidesImgList.clientWidth * direction;
+       carrosselTwoSlidesImgList.scrollBy({ left: scrollAmount, behavior: "smooth"});
+    });
+  });
+
+  const handeSlideButton = () => {
+    slideButtons[0].style.display = carrosselTwoSlidesImgList.scrollLeft <= 0 ? "none" : "block";
+    slideButtons[1].style.display = carrosselTwoSlidesImgList.scrollLeft >= maxScrollLeft ? "none" : "block";
+  }
+
+  const updateScrollThumbPosition = () => {
+    const scrollPosition = carrosselTwoSlidesImgList.scrollLeft;
+    const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+    scrollbarThumb.style.left = `${thumbPosition}px`;
+  }
+  
+  carrosselTwoSlidesImgList.addEventListener("scroll", () => {
+    handeSlideButton();
+    updateScrollThumbPosition();
+    });
+
+}
+
+window.addEventListener("load", initSlider);
+
+
+//show me categorys 
+
+function mostrarCategoria(categoria) {
+  var conteudo = document.getElementById('conteudo');
+  var categorias = conteudo.children;
+
+  for (var i = 0; i < categorias.length; i++) {
+    categorias[i].style.display = categorias[i].id === categoria ? 'block' : 'none';
+  }
+}
